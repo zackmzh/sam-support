47f6d057a6fc858ac044f8251fa413aaca5148b1
RICO MAI <ricomai@ricos-mac.home>
2017-09-14 19:23:32 -0400

    add sub catalog browsing component

--- src/app/route-head/route-head.module.ts
+++ src/app/route-head/route-head.module.ts
@@ -10,6 +10,9 @@ import { FindASolutionComponent } from './find-a-solution/find-a-solution.compon
 import { AllServiceComponent } from './find-a-solution/all-service/all-service.component';
 import { ProductSelectComponent } from './find-a-solution/product-select/product-select.component';
 import { GetToKnowYourProductComponent } from './get-to-know-your-product/get-to-know-your-product.component';
+import { CatalogBrowsingComponent } from './get-to-know-your-product/catalogBrowsing/catalogBrowsing.component';
+import { PremiumCareComponent } from './get-to-know-your-product/premium_care/premium_care.component';
+import { TypeBrowsingComponent } from './get-to-know-your-product/typeBrowsing/typeBrowsing.component';
 
 @NgModule({
   imports: [
@@ -23,7 +26,10 @@ import { GetToKnowYourProductComponent } from './get-to-know-your-product/get-to
     FindASolutionComponent,
     AllServiceComponent,
     ProductSelectComponent,
-    GetToKnowYourProductComponent
+    GetToKnowYourProductComponent,
+    CatalogBrowsingComponent,
+    PremiumCareComponent,
+    TypeBrowsingComponent
   ],
   exports: [
     RouteHeadComponent,
